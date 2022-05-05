7c64f77028be2a73c5e313fcc2ea6c5f134f9a67
Chris Wallace <cew54@cam.ac.uk>
2022-05-05 12:07:03 +0100

    update

--- makebib.sh
+++ makebib.sh
@@ -8,5 +8,7 @@ References
 EOD
 cat ~/Dropbox/Words/cv/me.bib | \
     grep -v '^@Comment' | \
-    sed 's/\\textbf{Wallace C}/Wallace C/'  >> _bibliography/references.bib
+    sed 's/\\textbf{Wallace C}/Wallace C/'  | \
+    sed 's/{\*Wallace C}/Wallace C/'  | \
+    sed 's/\*//'  >> _bibliography/references.bib
 
